a7aaf3e1c7419ff089afc10886431c7af5ffb244
B.ziva8702505 <zivarab@code.edu.az>
2022-07-20 07:13:54 +0400

    first

---
+++ reactapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="main">
+<Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+    <header>
+        <h2>My First React App</h2>
+    </header>
+    )
+  }
+}
+
+export default Navbar
